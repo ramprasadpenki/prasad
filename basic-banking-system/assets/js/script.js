6e11c0495d5312a66c03ef19eac8f07490042e4f
Your Name <ramprasadpenki56@gmail.com>
2021-06-13 17:11:57 +0530

    my project

--- basic-banking-system/assets/js/script.js
+++ basic-banking-system/assets/js/script.js
@@ -1,26 +1,28 @@
-let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
-
 function sendMoney(){
    var enterName = document.getElementById("enterName").value;
    var enterAmount = parseInt(document.getElementById("enterAmount").value);
-
-   if (enterAmount > 8000) {
+var enterSName = document.getElementById("enterSName").value;
+ var findSenderBankAccount = enterSName + "BankBalance";
+   var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+   if (enterAmount > enterSAmount) {
       alert("Insufficient Balance.")
-   } else {
+   }
+else {
       var findUserBankAccount = enterName + "BankBalance";
+     
       var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
-      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
-      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      var myAccountBalance = parseInt(document.getElementById(findSenderBankAccount).innerHTML) - enterAmount;
+      document.getElementById(findSenderBankAccount).innerHTML = myAccountBalance;
       document.getElementById(findUserBankAccount).innerHTML = finalAmount;
       alert(`Successful Transaction !!  
       $${enterAmount} is sent to ${enterName}@email.com.`)
 
       // transaction history 
       var createPTag = document.createElement("li");
-      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      var textNode = document.createTextNode(`Rs ${enterAmount} is sent from the sender with Email-id ${enterSName}@email.com
+ to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
       createPTag.appendChild(textNode);
       var element = document.getElementById("transaction-history-body");
       element.insertBefore(createPTag, element.firstChild);
    }
 }
-
